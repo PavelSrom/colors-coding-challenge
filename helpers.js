75807eb4bffdf40941078223b7a6fd1beb533dd1
Pavel Srom <srompavel98@gmail.com>
2021-12-01 19:46:53 +0100

    chore(app): add comments

--- helpers.js
+++ helpers.js
@@ -2,49 +2,62 @@ const { getColor } = require("./apiMock")
 
 const availableColors = ["red", "green", "blue", "black", "white"]
 
+// 'colorsInput' can be a single color, 'all' or colors separated by spaces
 const validateColors = (colorsInput) => {
   let colors
   let error = false
 
+  // if 'all' is provided, there's no error and return all colors
   if (colorsInput.toLowerCase() === "all")
     return { error, colors: availableColors }
 
+  // colors separated by spaces => create an array from input
   if (colorsInput.includes(" ")) {
     colors = colorsInput.trim().split(" ")
   } else {
     colors = colorsInput
   }
 
+  // if array (multiple colors), loop through and check against the ones we use
   if (Array.isArray(colors)) {
     for (const color of colors) {
       if (!availableColors.includes(color)) error = true
     }
   } else {
+    // only single color - check if 'availableColors' has it
     error = !availableColors.includes(colors)
   }
 
+  // return error and formatted color(s) back
   return { error, colors }
 }
 
+// self-explanatory
 const validateColorFormat = (format) => ["all", "HEX", "RGB"].includes(format)
 const validateSortFormat = (sort) => ["none", "asc", "desc"].includes(sort)
 const validateSyncAsync = (syncChoice) => ["sync", "async"].includes(syncChoice)
 
 const sortColors = (colors, sort) => {
+  // if no sort provided, return colors as is
   if (sort === "none") return colors
 
+  // if 'asc', sort from A-Z, otherwise sort from Z-A
   return colors.sort((a, b) =>
     sort === "asc" ? a.localeCompare(b) : b.localeCompare(a)
   )
 }
 
 const fetchColorAndFormat = async (color, format) => {
+  // fetch color from api mock
   const targetColor = await getColor(color)
+  // if no format or 'all', return the color as is
   if (!format || format === "all") return targetColor
 
+  // otherwise return the corresponding format for the color
   return targetColor[format]
 }
 
+// self-explanatory
 const endReadline = (rl) => {
   console.log("Oops! You inputted the wrong information :(")
   rl.close()
--- index.js
+++ index.js
@@ -18,12 +18,14 @@ const {
  * 4. only multiple colors - synchronous or not?
  */
 
+// create a readline instance
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 })
 
 // https://stackoverflow.com/questions/46907761/node-js-promisify-readline
+// promisify readline inputs so that we can use async/await
 const userInput = (query) => {
   return new Promise((resolve) => {
     rl.question(query, resolve)
@@ -36,48 +38,55 @@ const userInput = (query) => {
 
 const main = async () => {
   const colorsInput = await userInput(
+    // ask first question
     "What colors do you want? If all colors, type 'all', if multiple, please separate by spaces: "
   )
-  const { error, colors } = validateColors(colorsInput)
-  if (error) return endReadline(rl)
+  const { error, colors } = validateColors(colorsInput) // validate answer
+  if (error) return endReadline(rl) // if invalid answer, stop execution
 
   const colorFormatInput = await userInput(
+    // ask second question
     "What format do you want returned? Choose between 'all' / 'HEX' / 'RGB': "
   )
-  const isCorrectColorFormat = validateColorFormat(colorFormatInput)
-  if (!isCorrectColorFormat) return endReadline(rl)
+  const isCorrectColorFormat = validateColorFormat(colorFormatInput) // validate answer
+  if (!isCorrectColorFormat) return endReadline(rl) // if invalid answer, stop execution
 
   // if we have multiple colors going on, the program continues
   if (Array.isArray(colors)) {
     const sortOrderInput = await userInput(
+      // ask third question
       "How do you want the colors to be sorted? Choose 'asc' / 'desc' / 'none': "
     )
-    const isCorrectSortFormat = validateSortFormat(sortOrderInput)
-    if (!isCorrectSortFormat) return endReadline(rl)
+    const isCorrectSortFormat = validateSortFormat(sortOrderInput) // validate answer
+    if (!isCorrectSortFormat) return endReadline(rl) // if invalid answer, stop execution
 
-    const colorsAfterSort = sortColors(colors, sortOrderInput)
+    const colorsAfterSort = sortColors(colors, sortOrderInput) // sort colors
 
     const syncOrAsyncInput = await userInput(
+      // ask fourth question
       "How do you want the colors to be fetched? Choose 'sync' / 'async': "
     )
-    const isCorrectSyncFormat = validateSyncAsync(syncOrAsyncInput)
-    if (!isCorrectSyncFormat) return endReadline(rl)
+    const isCorrectSyncFormat = validateSyncAsync(syncOrAsyncInput) // validate answer
+    if (!isCorrectSyncFormat) return endReadline(rl) // if invalid answer, stop execution
 
     if (syncOrAsyncInput === "sync") {
+      // if sync order, loop through colors, fetch one by one and log to terminal
       for (const color of colorsAfterSort) {
         const targetColor = await fetchColorAndFormat(color, colorFormatInput)
         console.log("Here's your winner:")
         console.log(targetColor)
       }
     } else {
+      // otherwise create a list of promises
       const listOfPromises = []
-
       for (const color of colorsAfterSort) {
+        // push each fetch call for the color to the list
         listOfPromises.push(fetchColorAndFormat(color, colorFormatInput))
       }
-
+      // fetch all colors simultaneously
       const allColors = await Promise.all(listOfPromises)
       console.log("Here's your winner:")
+      // format answer
       console.log(JSON.stringify(allColors, null, 2))
     }
   } else {
@@ -86,7 +95,7 @@ const main = async () => {
     console.log("Here's your winner:")
     console.log(targetColor)
   }
-
+  // exit terminal
   rl.close()
 }
 
